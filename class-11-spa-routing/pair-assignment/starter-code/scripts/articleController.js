92e1e7c35bf64d12fd94f29ab11ea1320fd5909d
Caleb Sattgast <calebsattgast@gmail.com>
2016-04-25 13:08:58 -0700

    move Article.createTable call outside of articlesController.index scope

--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -1,14 +1,14 @@
 (function(module) {
   var articlesController = {};
 
-  // TODO: Create the `articles` table when the controller first loads,
+  // DONE: Create the `articles` table when the controller first loads,
   //  with the code that used to be in index.html:
+  Article.createTable();
 
   articlesController.index = function() {
     // DONE: Complete this function that kicks off the fetching and rendering
     //  of articles, using the same
     //  code that used to be in index.html:
-    Article.createTable();
     Article.fetchAll(articleView.initIndexPage);
 
     // DONE: But wait! There's more: Also be sure to hide all the main section
