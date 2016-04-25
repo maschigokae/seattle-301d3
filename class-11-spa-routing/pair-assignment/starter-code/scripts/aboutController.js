ac75d8fc8d32ea069d1dca523af15fd990a3cc1c
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-04-25 14:07:50 -0700

    completed to do on aboutController.js

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -3,10 +3,7 @@
 
   aboutController.index = function() {
     // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-    
-    //note: temp placeholders below...
-    $('most-stuff').hide();
-    $('#about').show();
+    $('#about').show().siblings().hide();
   };
 
   module.aboutController = aboutController;
