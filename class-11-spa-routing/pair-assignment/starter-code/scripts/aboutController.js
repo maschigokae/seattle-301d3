821a1c49a923abc567c130d0086abaf98f28582c
Caleb Sattgast <calebsattgast@gmail.com>
2016-04-25 12:55:13 -0700

    add jQuery PLACEHOLDERS to about route controller

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -3,6 +3,10 @@
 
   aboutController.index = function() {
     // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+    
+    //note: temp placeholders below...
+    $('most-stuff').hide();
+    $('#about').show();
   };
 
   module.aboutController = aboutController;
